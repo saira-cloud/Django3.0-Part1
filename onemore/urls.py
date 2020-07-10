627e0f087fd245487135781dfa033b7f284276ee
hassaan qazi <sairaiqbal737@gmail.com>
2020-07-11 04:31:12 +0500

    my django work part 1

---
+++ onemore/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+
+
+urlpatterns = [
+     path('admin/', admin.site.urls),
+     path('', include('accounts.urls')),
+   
+    
+]
+
+
+
